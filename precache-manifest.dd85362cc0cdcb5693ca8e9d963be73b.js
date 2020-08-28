86ee9982d4a3d670e2f1c979b361d8f4eec05b37
Robert Higdon <robert@ulastnight.com>
2020-08-28 15:05:41 -0700

    Updates

---
+++ precache-manifest.dd85362cc0cdcb5693ca8e9d963be73b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "946d4d511f96fd7cbe6239d47e6801be",
+    "url": "/bunnyhawk/covid-memorial/index.html"
+  },
+  {
+    "revision": "1c22520c361b15ae7b64",
+    "url": "/bunnyhawk/covid-memorial/static/css/main.127be1ce.chunk.css"
+  },
+  {
+    "revision": "40c0752a21ea781e42f1",
+    "url": "/bunnyhawk/covid-memorial/static/js/2.b57481fb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/bunnyhawk/covid-memorial/static/js/2.b57481fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c22520c361b15ae7b64",
+    "url": "/bunnyhawk/covid-memorial/static/js/main.c2064c10.chunk.js"
+  },
+  {
+    "revision": "505842f994c0f3e4fb25",
+    "url": "/bunnyhawk/covid-memorial/static/js/runtime-main.406281c3.js"
+  }
+]);
